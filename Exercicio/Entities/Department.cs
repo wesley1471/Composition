465dc4c191d67f49731b2b5e229931710bdf49b1
Wesley <wesley1471@gmail.com>
2023-01-07 12:02:11 -0300

    New folder: Entities

---
+++ Exercicio/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercicio.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Exercicio/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace Exercicio.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
